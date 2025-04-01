638274a9320f5d230b3a71c0d76852590622c3bc
Bubbiecloudy <123pettyhappy@gmail.com>
2025-04-01 16:33:45 +0700

    Update Bai15.java

--- Bai15.java
+++ Bai15.java
@@ -1,36 +1,19 @@
-package Lab1;
 import java.util.Scanner;
 
-public class Bai15 {
-    public static void tachNgayThangNam(String ngay) {
-        ngay = ngay.trim();
-
-        int ngaySo = 0, thangSo = 0, namSo = 0;
-
-        String[] phanTach = ngay.split("/");
-
-        for (int i = 0; i < phanTach.length; i++) {
-            if (i == 0) {
-                ngaySo = Integer.parseInt(phanTach[i]);
-            } else if (i == 1) {
-                thangSo = Integer.parseInt(phanTach[i]);
-            } else if (i == 2) {
-                namSo = Integer.parseInt(phanTach[i]);
-            }
-        }
-
-        System.out.println("Ngay: " + ngaySo);
-        System.out.println("Thang: " + thangSo);
-        System.out.println("Nam: " + namSo);
-    }
-
+public class TachNgayThangNam {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhap ngay (dd/mm/yyyy): ");
-        String ngay = scanner.nextLine();
+        String ngay = scanner.nextLine().trim();
 
-        tachNgayThangNam(ngay);
+        String[] phanTach = ngay.split("/");
+        System.out.println("Ngay: " + phanTach[0]);
+        System.out.println("Thang: " + phanTach[1]);
+        System.out.println("Nam: " + phanTach[2]);
+
+        scanner.close();
     }
 }
+
 
