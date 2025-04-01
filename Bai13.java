a28923e05c4de865d7080295b030f864716ece3d
Bubbiecloudy <123pettyhappy@gmail.com>
2025-04-01 16:37:25 +0700

    Update Bai13.java

--- Bai13.java
+++ Bai13.java
@@ -2,34 +2,19 @@ package Lab1;
 import java.util.Scanner;
 
 public class Bai13 {
-    public static int demSoTu(String chuoi) {
-        int dem = 0;
-        boolean trongTu = false;
-
-        for (int i = 0; i < chuoi.length(); i++) {
-            char kiTu = chuoi.charAt(i);
-
-            if (kiTu == ' ') {
-                trongTu = false;
-            } else {
-                if (!trongTu) {
-                    dem++;
-                    trongTu = true;
-                }
-            }
-        }
-
-        return dem;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhap chuoi: ");
         String chuoi = scanner.nextLine();
 
-        int soTu = demSoTu(chuoi);
+        String[] tu = chuoi.split(" ");
+
+        int soTu = tu.length;
+
         System.out.println("So tu trong chuoi: " + soTu);
+
+        scanner.close();
     }
 }
 
